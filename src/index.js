16481549d05058a4f752a11b4e0ea515fc2e0019
Igor Napolov <isnapolov@gmail.com>
2021-12-06 09:14:38 +0200

    добавление опции debounce и CSS

--- src/index.js
+++ src/index.js
@@ -16,14 +16,14 @@ searchBoxEl.addEventListener('input',debounce(onSearch,DEBOUNCE_DELAY));
 function onSearch(event) {
     event.preventDefault();
     
-    const formInput = event.currentTarget;    
+    const formInput = event.target;    
     const searchQuery = formInput.value;
     
     if (searchQuery==="") {
         clearCountriesContainer();
         return;
-    }
-
+    };
+    
     fetchCountriesName(searchQuery.trim())
         .then(countries => {
             console.log(countries.length);
@@ -47,8 +47,8 @@ function onSearch(event) {
 function renderCountryList(countries) {
     const markup = countries
         .map(({ name,flags }) => {
-            return `<li>
-      <img src=${flags.svg} width="50px" alt="${name.official}">
+            return `<li class="country-list__item">
+      <img src=${flags.svg} width="50px" height="30px" alt="${name.official}">
           <h2 class="name">${name.official}</h2>
           
         </li>`;
@@ -62,15 +62,15 @@ function renderCountryList(countries) {
 function renderCountryCard(countries) {    
     const markup = countries
         .map(({ name,capital,population,flags,languages }) => {
-            return `<div class="counrty-card">
-            <div class="flag">
-      <img src=${flags.svg} width="50px" alt="${name.official}">
+            return `<div class="country-card">
+            <div>
+      <img src=${flags.svg} width="50px" height="30px" alt="${name.official}">
       </div>
-          <h1 class="name">${name.official}</h2>
+          <h1 class="country-card__name">${name.official}</h2>
           </div>
-          <p><span>Capital: </span>${capital}</p>
-          <p><span>Population: </span>${population}</p>
-          <p><span>Languages: </span>${Object.values(languages).join(", ")}</p>
+          <p><span class="country-property">Capital: </span>${capital}</p>
+          <p><span class="country-property">Population: </span>${population}</p>
+          <p><span class="country-property">Languages: </span>${Object.values(languages).join(", ")}</p>
         `;
     })
     .join("");
